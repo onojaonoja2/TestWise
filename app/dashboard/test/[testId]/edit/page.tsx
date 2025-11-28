d73f22a5ca2b26aef7d1c0e92fa58b6a0e77f90c
onojaonoja2 <samuelonoja06@gmail.com>
2025-11-28 11:15:49 +0100

    feat: Add new paddings for creating and editing tests within the dashboard.

--- app/dashboard/test/[testId]/edit/page.tsx
+++ app/dashboard/test/[testId]/edit/page.tsx
@@ -437,7 +437,7 @@ export default function EditTestPage({ params }: { params: Promise<{ testId: str
                                                 setNewQ({ ...newQ, options: newOptions })
                                             }}
                                             placeholder={`Option ${i + 1}`}
-                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            className="block w-full rounded-md border-0 py-1.5 pl-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         />
                                     ))}
                                 </div>
